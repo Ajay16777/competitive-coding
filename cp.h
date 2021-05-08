564599beca2f003227f914d1789d2bd4469d72e6
Ajay Singh <64024440+Ajay16777@users.noreply.github.com>
2021-05-08 16:48:26 +0530

    Add files via upload
    you can add this file on header on your c++ program as #include<cp.h>
    this well help you to see input window , error window, and output window while you coding you just create 3 files in your program directory(where you save your all c++ program )and copy cp.h file in C:\MinGW\include and there you go

---
+++ cp.h
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("error.txt", "w", stderr);
+freopen("output.txt", "w", stdout);
+#endif
+
+
+solve();
+
+cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+return 0;
+}
